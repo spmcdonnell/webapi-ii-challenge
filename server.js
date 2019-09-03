8deb11fbd282cada2cac3e8290fbaf593a4bf229
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-03 15:49:17 -0400

    Add methods for adding a new post a retrieving all posts

--- server.js
+++ server.js
@@ -6,13 +6,35 @@ const server = express();
 server.use(express.json());
 
 // POST
-server.post('/api/posts', (req, res) => {});
+server.post('/api/posts', (req, res) => {
+    const newPost = req.body;
+
+    if (newPost.title && newPost.contents) {
+        db.insert(newPost)
+            .then(post => {
+                res.status(201).json(post);
+            })
+            .catch(error => {
+                res.status(500).json({ error: error, message: 'There was an error while saving the post to the database' });
+            });
+    } else {
+        res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+    }
+});
 
 server.post('/api/posts/:id/comments', (req, res) => {});
 
 // GET
 
-server.get('/api/posts', (req, res) => {});
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error, message: 'The posts information could not be retrieved.' });
+        });
+});
 
 server.get('/api/posts/:id', (req, res) => {});
 
